131955f9ff7cc99149248308142653e73bbd2bc3
Mustafa Mert Tunalı <tunalim@mef.edu.tr>
2023-01-02 03:45:07 +0300

    ADD: Create and set main file to manage experiments

--- src/graph.py
+++ src/graph.py
@@ -1,20 +1,62 @@
-from typing import Dict, List
+import matplotlib.pyplot as plt
+
+from min_heap import MinHeap
+from typing import Dict, List, Tuple
+
+from utils import visualize_graph, visualize_shortest_path
 
 
 class Graph(MinHeap):
     def __init__(self):
         self.graph = {}
+        self.node_coordinates = {}
+        self.a_star_parents = {}
+        self.dijkstra_parents = {}
 
-    def add_node(self, node: int):
+    def add_node(self, node: int, x: int, y: int):
         self.graph[node] = []
+        self.node_coordinates[node] = (x, y)
+
+    def get_node(self, node: int):
+        return self.node_coordinates[node]
+
+    def get_nodes(self) -> List[int]:
+        return list(self.graph.keys())
+
+    def get_node_coordinates(self, node: int) -> Tuple[int, int]:
+        return self.node_coordinates[node]
+
+    def get_edge_weight(self, node1: int, node2: int) -> int:
+        for neighbor, weight in self.graph[node1]:
+            if neighbor == node2:
+                return weight
+        return float("inf")
 
     def add_edge(self, node1: int, node2: int, weight: int):
         self.graph[node1].append((node2, weight))
         self.graph[node2].append((node1, weight))
 
-    def dijkstra(self, source: int, target: int) -> int:
+    def get_edges(self) -> List[Tuple[int, int]]:
+        edges = []
+        for node in self.graph.keys():
+            for neighbor, _ in self.graph[node]:
+                if (node, neighbor) not in edges:
+                    edges.append((node, neighbor))
+        return edges
+
+    def has_edge(self, node1: int, node2: int) -> bool:
+        for neighbor, _ in self.graph[node1]:
+            if neighbor == node2:
+                return True
+        return False
+
+    def get_neighbors(self, node: int) -> List[int]:
+        return [neighbor for neighbor, _ in self.graph[node]]
+
+    def dijkstra(self, source: int, target: int) -> Tuple[List[int], int]:
         distances = {node: float("inf") for node in self.graph.keys()}
         distances[source] = 0
+        self.dijkstra_parents = {source: None}  # initialize the parent mapping
 
         self.heap = []
         self.push((0, source))  # (distance, node)
@@ -23,11 +65,134 @@ class Graph(MinHeap):
             distance, node = self.pop()
 
             if node == target:
-                return distance
+                path = [target]
+                while path[-1] != source:
+                    path.append(self.dijkstra_parents[path[-1]])
+                path.reverse()
+                return path, distance
 
             for neighbor, weight in self.graph[node]:
                 new_distance = distance + weight
                 if new_distance < distances[neighbor]:
                     distances[neighbor] = new_distance
+                    self.dijkstra_parents[neighbor] = node
                     self.push((new_distance, neighbor))
-        return float("inf")
+        return [], float("inf")
+
+    # def h_func(self, node: int, target: int) -> int:
+    #     """This heuristic function is similar to the Manhattan distance,
+    #     but it allows for diagonal movement between nodes.
+
+    #     Args:
+    #         node (int): TODO
+    #         target (int): TODO
+
+    #     Returns:
+    #         int: TODO
+    #     """
+    #     node_x, node_y = self.get_node_coordinates(node)
+    #     target_x, target_y = self.get_node_coordinates(target)
+    #     dx = abs(node_x - target_x)
+    #     dy = abs(node_y - target_y)
+    #     return max(dx, dy) + (np.sqrt(2) - 1) * min(dx, dy)
+
+    # def h_func(self, node: int, target: int) -> int:
+    #     """Manhattan distance: This heuristic function calculates
+    #     the distance between two nodes as the sum of the absolute
+    #     differences of their coordinates.
+
+    #     Args:
+    #         node (int): TODO
+    #         target (int): TODO
+
+    #     Returns:
+    #         int: TODO
+    #     """
+    #     node_x, node_y = self.get_node_coordinates(node)
+    #     target_x, target_y = self.get_node_coordinates(target)
+    #     return abs(node_x - target_x) + abs(node_y - target_y)
+    def h_func(self, node: int, target: int) -> int:
+
+        """This heuristic function estimates the distance between
+        the two nodes by taking the absolute value of the difference
+        between their indices.
+
+        Args:
+            node (int): _description_
+            target (int): _description_
+
+        Returns:
+            int: _description_
+        """
+        return abs(target - node)
+
+    def a_star(self, source: int, target: int) -> Tuple[List[int], int]:
+        distances = {node: float("inf") for node in self.graph.keys()}
+        distances[source] = 0
+        self.a_star_parents = {source: None}  # initialize the parent mapping
+
+        open_set = set([source])
+        closed_set = set()
+
+        while open_set:
+            node = min(
+                open_set, key=lambda x: distances[x] + self.h_func(x, target)
+            )  # f(n) = g(n) + h(n), looking for the node with the lowest f(n)
+
+            if node == target:
+                path = [target]
+                while path[-1] != source:
+                    path.append(self.a_star_parents[path[-1]])
+                path.reverse()
+                return path, distances[target]
+
+            open_set.remove(node)
+            closed_set.add(node)
+
+            for neighbor, weight in self.graph[node]:
+                if neighbor in closed_set:
+                    continue
+                new_distance = distances[node] + weight
+
+                if new_distance < distances[neighbor]:
+                    distances[neighbor] = new_distance
+                    self.a_star_parents[neighbor] = node
+                    open_set.add(neighbor)
+
+        return [], float("inf")
+
+    def __str__(self):
+        return str(self.graph)
+
+
+if __name__ == "__main__":
+
+    g = Graph()
+
+    # 4 nodes, 5 edges
+    g.add_node(1, 0, 0)
+    g.add_node(2, 0, 1)
+    g.add_node(3, 1, 0)
+    g.add_node(4, 1, 1)
+
+    g.add_edge(1, 2, 8)
+    g.add_edge(1, 3, 1)
+    g.add_edge(2, 4, 2)
+    g.add_edge(3, 4, 3)
+    g.add_edge(1, 4, 2)
+
+    print(g)
+    djikstra_distance = g.dijkstra(1, 4)
+    print(f"Dijkstra distance: {djikstra_distance}")
+
+    a_star_distance = g.a_star(1, 4)
+    print(f"A* distance: {a_star_distance}")
+
+    nodes = list(g.graph.keys())
+    edges = []
+    for node, neighbors in g.graph.items():
+        for neighbor, weight in neighbors:
+            edges.append((node, neighbor))
+
+    # visualize_graph(g, nodes, edges)
+    visualize_shortest_path(g, nodes, edges, 1, 4, "dijkstra")
---
+++ src/main.py
@@ -0,0 +1,75 @@
+import random
+import argparse
+
+from graph import Graph
+from utils import initialize_graph, visualize_graph, visualize_shortest_path
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--compare", action="store_true", help="compare the results")
+
+args = parser.parse_args()
+
+
+print(
+    f"Welcome to the COMP 303, Project 2 Assignment!\nThe program initializes a graph with random edges and weights.\nIf target node is not reachable from source node, the program will print 'No path found!'.\n"
+)
+
+N = int(input("Enter the number of nodes: "))
+
+if not args.compare:
+    if N < 2 or N > 20:
+        print(
+            "Number of nodes must be between 2 and 20. For higher N, python main.py --compare=True"
+        )
+        exit(1)
+
+print(f"Graph is creating with {N} nodes...", end="")
+
+g = initialize_graph(Graph(), N)
+
+if args.compare:
+    source = 1
+    target = N
+    # results = g.compare_algorithms(source, target)
+    # for algo_type, result in results.items():
+    #     print(
+    #         f"Algorithm {algo_type} found the shortest path: {result[0]} with distance {result[1]} and the total number of repetitions are {result[2]}."
+    #     )
+    exit(1)
+
+# visualize_graph(g, g.get_nodes(), g.get_edges())
+
+shortest_path_algorithms = {0: g.dijkstra, 1: g.a_star}
+while True:
+    algorithm = int(
+        input(
+            "Select the algorithm [0 for djkstra, 1 for A*, ] or compare the algorihms (2): "
+        )
+    )
+    try:
+        source, target = int(input("Enter the source node: ")), int(
+            input("Enter the target node: ")
+        )
+        # visualize_shortest_path(
+        #     g, g.get_nodes(), g.get_edges(), source, target, algorithms[algorithm]
+        # )
+
+        shortest_path, distance = shortest_path_algorithms[algorithm](source, target)
+        print(
+            f"Shortest path from {source} to {target} is {shortest_path} with distance {distance}."
+        )
+
+        # TODO: Running time
+
+        # TODO: Total number of repetitions
+        # print(f"Total number of repetitions: {g.repetitions}")
+
+        # Theoretical running time of the algorithm
+        print(f"Theoretical running time of the algorithm: O(n^2)")
+
+        break
+    except KeyError:
+        print("No path found!")
+        continue
